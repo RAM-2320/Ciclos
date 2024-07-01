017b7a57de452f93fd3ade31d9fd4e4a44f645b9
RAM-2320 <Rogerarmandomelomontero@gmail.com>
2024-07-01 17:42:29 -0400

    Add files via upload

---
+++ 10. Leer un número entero y determinar a cuánto es igual la suma de todos los enteros comprendidos entre 1 y el número leído..py
@@ -0,0 +1,5 @@
+numero = int(input("Ingrese un número entero: "))
+suma = 0
+for i in range(1, numero + 1):
+    suma += i
+print("La suma de los enteros comprendidos entre 1 y", numero, "es:", suma)
